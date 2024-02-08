053a7e647080b4dd9467fdd381b4476366bf712a
samuelHdzMurillo <samuelop99op@gmail.com>
2024-02-08 13:49:15 -0700

    cambios en import para que agarre mas datos y de coordenadas

--- app/Imports/PromotedImport.php
+++ app/Imports/PromotedImport.php
@@ -6,6 +6,7 @@ use App\Models\Promoted;
 use App\Models\Section;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Http; // Importar la facade Http
 
 class PromotedImport implements ToModel, WithHeadingRow
 {
@@ -15,6 +16,7 @@ class PromotedImport implements ToModel, WithHeadingRow
     {
         $this->promotorId = $promotorId;
     }
+
     /**
      * @param array $row
      *
@@ -22,19 +24,39 @@ class PromotedImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
+        $direccionCompleta = $row['numero_ext'] . ', ' . $row['colonia'] . ', ' . $row['ciudad'] . ', ' . $row['estado'] . ', ' . 'Mexico';
+        $response = Http::get("https://nominatim.openstreetmap.org/search", [
+            'q' => $direccionCompleta,
+            'format' => 'json',
+            'limit' => 1
+        ]);
+
+        $latitude = null;
+        $longitude = null;
+
+        if ($response->successful() && count($response->json()) > 0) {
+            $geocodeData = $response->json()[0];
+            $latitude = $geocodeData['lat'];
+            $longitude = $geocodeData['lon'];
+        }
+
         $section = Section::where("number", $row["section"])->first();
+
         return new Promoted([
-            'name'           => $row['name'],
-            'last_name'      => $row['last_name'],
-            'phone_number'   => $row['phone_number'],
-            'email'          => $row['email'],
-            'adress'         => $row['adress'],
-            'electoral_key'  => $row['electoral_key'],
-            'curp'           => $row['curp'],
-            'latitude'       => $row['latitude'],
-            'longitude'      => $row['longitude'],
-            "section_id"    => "$section->id",
-            'promotor_id'   => "$this->promotorId",
+            'name'          => $row['nombre'],
+            'last_name'     => $row['apellidos'],
+            'phone_number'  => $row['numero_telefonico'],
+            'email'         => $row['correo'],
+            'adress'        => $row['dirrecion'],
+            'electoral_key' => $row['clave_electoral'],
+            'curp'          => $row['curp'],
+            'latitude'      => $latitude, // Seteado desde la geocodificación
+            'longitude'     => $longitude, // Seteado desde la geocodificación
+            'colony'        => $row['colonia'],
+            'postal_code'   => $row['codigo_postal'],
+            'house_number'  => $row['numero_ext'],
+            'section_id'    => $section ? $section->id : null,
+            'promotor_id'   => $this->promotorId,
         ]);
     }
 }
