60fb5d4784864f3758f0047fc195954ad7a3de43
danieldarkmun20 <daniel_200200@live.com.mx>
2024-02-24 18:36:34 -0700

    Refactor GoalDistrictController and GoalSectionController***

--- app/Http/Controllers/GoalDistrictController.php
+++ app/Http/Controllers/GoalDistrictController.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 
 class GoalDistrictController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         $query = GoalDistrict::with('district.sections.promoteds');
 
